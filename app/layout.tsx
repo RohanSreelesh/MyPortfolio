ba862ca616ef8b5fe48b8916f5821afb04cb9fa3
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2023-08-13 14:46:48 -0400

    Added head to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 import Navbar from './components/Navbar'
 import Footer from "./components/Footer"
+import Head from './head'
 
 
 
@@ -26,7 +27,7 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <Head />
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
